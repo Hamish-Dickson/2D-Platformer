4b4a417a3aef2700cf65871521524a1129dd8ab1
Hamish <Hamishdickson98@gmail.com>
2019-03-22 00:22:04 +0000

    final pass at commenting and formatting

--- src/Game.java
+++ src/Game.java
@@ -60,15 +60,13 @@ public class Game extends GameCore {
      * @param args The list of parameters this program might use (ignored)
      */
     public static void main(String[] args) {
-        Sound s = new Sound("sounds/song.wav");
-
+        Sound s = new Sound("sounds/song.wav");//load background music to be played
         s.start();
+
         Game gct = new Game();
         gct.init("map1.txt");
         // Start in windowed mode with the given screen height and width
         gct.run(false, screenWidth, screenHeight);
-
-
     }
 
     /**
@@ -80,7 +78,7 @@ public class Game extends GameCore {
         // Load the tile map and print it out so we can check it is valid
         tmap.loadMap("maps", mapFile);
 
-
+        //create animations for player and enemies
         playerAnim = new Animation();
         playerAnim.addFrame(loadImage("images/sprites/player_pause1.png"), animDuration);
         playerAnim.addFrame(loadImage("images/sprites/player_pause2.png"), animDuration);
@@ -99,10 +97,10 @@ public class Game extends GameCore {
         enemy2 = new Sprite(enemyAnim);
         enemy3 = new Sprite(enemyAnim);
 
-
+        //initialise the game world
         initialiseGame();
 
-        System.out.println(tmap);
+        System.out.println(tmap);//output to show the map
     }
 
     /**
@@ -111,6 +109,7 @@ public class Game extends GameCore {
      * the game.
      */
     public void initialiseGame() {
+        //position and show the player
         player.setX(20);
         player.setY(100);
         player.setVelocityX(0);
@@ -124,9 +123,12 @@ public class Game extends GameCore {
         enemy3.show();
     }
 
+    /**
+     * Method to initially setup the enemy locations, based on the level
+     */
     private void setupEnemies() {
         switch (level) {
-            case 1:
+            case 1://if the level is 1
                 enemy1.setX(230);
                 enemy2.setX(400);
                 enemy3.setX(800);
@@ -135,7 +137,7 @@ public class Game extends GameCore {
                 enemy2.setY(185);
                 enemy3.setY(100);
                 break;
-            case 2:
+            case 2://if the level is 2
                 enemy1.setX(270);
                 enemy2.setX(450);
                 enemy3.setX(800);
@@ -144,7 +146,7 @@ public class Game extends GameCore {
                 enemy2.setY(200);
                 enemy3.setY(250);
                 break;
-            default:
+            default://if an unexpected value is assigned to level
                 enemy1.setX(230);
                 enemy2.setX(400);
                 enemy3.setX(800);
@@ -159,19 +161,10 @@ public class Game extends GameCore {
      * Draw the current state of the game
      */
     public void draw(Graphics2D g) {
-        // Be careful about the order in which you draw objects - you
-        // should draw the background first, then work your way 'forward'
-
         // First work out how much we need to shift the view
         // in order to see where the player is.
-        int xo = (int) -player.getX() + 20;
+        int xo = (int) -player.getX() + 20;//offset camera by 20 from where the player is
         int yo = 0;
-
-
-        // If relative, adjust the offset so that
-        // it is relative to the player
-
-        // ...?
 
         g.setColor(Color.white);
         g.fillRect(0, 0, getWidth(), getHeight());
@@ -188,10 +181,11 @@ public class Game extends GameCore {
         enemy3.draw(g);
 
         player.draw(g);
+
         // Apply offsets to tile map and draw  it
         tmap.draw(g, xo, yo);
 
-        // Show score and status information
+        // Show status information
         String msg = "Status: " + status;
         g.setColor(Color.darkGray);
         g.drawString(msg, getWidth() - 150, 50);
@@ -210,21 +204,22 @@ public class Game extends GameCore {
             }
             player.setAnimationSpeed(1.0f);
 
-            if (up) {
+            if (up) {//if the player is moving up
                 if (jumpsDone < 2) {//only allows for 2 jumps per landing i.e the player must land before they jump again
                     if (player.getVelocityY() >= 0) {//only allow jump when trajectory is downwards
                         player.setVelocityY(-0.20f);
                         player.shiftY(-0.01f);
-                        up = false;
+
+                        up = false;//reset up flag
                         updateAnim("up");
-                        Sound s = new Sound("sounds/jump.wav");
+                        Sound s = new Sound("sounds/jump.wav");//play jumping noise
                         s.start();
-                        jumpsDone++;
+                        jumpsDone++;//increment jumps done this jump
                     }
                 }
             }
 
-            if (left) {
+            if (left) {//if travelling left
                 if (player.getVelocityX() > 0.04f) {//slow down dramatically if trying to move in other direction
                     player.setVelocityX(0.04f);
                     left = false;
@@ -235,7 +230,7 @@ public class Game extends GameCore {
                 }
             }
 
-            if (right) {
+            if (right) {//if travelling right
                 if (player.getVelocityX() < -0.04f) {//slow down dramatically if trying to move in other direction
                     player.setVelocityX(-0.04f);
                     right = false;
@@ -245,21 +240,19 @@ public class Game extends GameCore {
                     updateAnim("right");
                 }
             }
-
+            //collect the enemies into an ArrayList for simple processing
             ArrayList<Sprite> enemies = new ArrayList<Sprite>();
             enemies.add(enemy1);
             enemies.add(enemy2);
             enemies.add(enemy3);
 
-            for (Sprite enemy : enemies) {
-                if ((enemy.getX() >850 && enemy.isDirection()) || (enemy.getX()<250 && !enemy.isDirection())){
-                    enemy.setDirection(!enemy.isDirection());
+            for (Sprite enemy : enemies) {//for each enemy
+                if ((enemy.getX() > 850 && enemy.isDirection()) || (enemy.getX() < 250 && !enemy.isDirection())) {//if the enemy is at the edge of the patrol area
+                    enemy.setDirection(!enemy.isDirection());//invert the direction boolean
                 }
-
-                if(enemy.isDirection()){
+                if (enemy.isDirection()) {//if the enemy is moving right
                     enemy.setVelocityX(0.04f);
-                }
-                else{
+                } else {//if the enemy is moving left
                     enemy.setVelocityX(-0.04f);
                 }
                 enemy.update(elapsed);
@@ -269,7 +262,7 @@ public class Game extends GameCore {
 
             // Then check for any collisions that may have occurred
             handleTileMapCollisions(player, elapsed);
-            
+
             //check for sprite collisions
             handleSpriteCollisions();
         } else {//if the game is over
@@ -279,59 +272,59 @@ public class Game extends GameCore {
     }
 
 
-
     private void updateAnim(String direction) {
         System.out.println(direction);
         switch (direction) {
-            case "left":
+            case "left"://if the left animation is requested
                 playerAnim = new Animation();
                 playerAnim.addFrame(loadImage("images/sprites/player_left1.png"), animDuration);
                 playerAnim.addFrame(loadImage("images/sprites/player_left2.png"), animDuration);
                 break;
 
-            case "right":
+            case "right"://if the right animation is requested
                 playerAnim = new Animation();
                 playerAnim.addFrame(loadImage("images/sprites/player_right1.png"), animDuration);
                 playerAnim.addFrame(loadImage("images/sprites/player_right2.png"), animDuration);
                 break;
-            case "up":
+            case "up"://if the jumping animation is requested
                 playerAnim = new Animation();
                 playerAnim.addFrame(loadImage("images/sprites/player_up1.png"), animDuration);
                 playerAnim.addFrame(loadImage("images/sprites/player_up2.png"), animDuration);
                 break;
-            case "dead":
+            case "dead"://if the death animation is requested
                 playerAnim = new Animation();
                 playerAnim.addFrame(loadImage("images/sprites/player_dead.png"), animDuration);
                 break;
-            case "pause":
+            case "pause"://if the paused animation is requested
                 playerAnim = new Animation();
                 playerAnim.addFrame(loadImage("images/sprites/player_pause1.png"), animDuration);
                 playerAnim.addFrame(loadImage("images/sprites/player_pause2.png"), animDuration);
                 break;
-            default:
+            default://default for bad arguments
                 playerAnim = new Animation();
                 playerAnim.addFrame(loadImage("images/sprites/player_pause1.png"), animDuration);
                 playerAnim.addFrame(loadImage("images/sprites/player_pause2.png"), animDuration);
                 break;
         }
-        player.setAnimation(playerAnim);
+        player.setAnimation(playerAnim);//set the animation to requested one
     }
 
     private void handleSpriteCollisions() {
+        //again group the enemies for easy processing
         ArrayList<Sprite> enemies = new ArrayList<Sprite>();
         enemies.add(enemy1);
         enemies.add(enemy2);
         enemies.add(enemy3);
 
-        boolean collided = false;
+        boolean collided = false;//has the player collided with any NPCs
 
-        for (Sprite enemy: enemies){//for each enemy
-            if(boundingBoxCollision(enemy, player)){
+        for (Sprite enemy : enemies) {//for each enemy
+            if (boundingBoxCollision(enemy, player)) {//if the player has collided with the enemy
                 collided = true;
             }
         }
 
-        if(collided){
+        if (collided) {//if a collision happened
             endGame();
         }
 
@@ -339,12 +332,14 @@ public class Game extends GameCore {
 
     /**
      * function to check if 2 sprites are colliding with each other
+     *
      * @param s1 first sprite to be checked
      * @param s2 second sprite to be checked
      * @return true if collision, else false
      */
     public boolean boundingBoxCollision(Sprite s1, Sprite s2) {
-        return ((s1.getX() + s1.getImage().getWidth(null) >s2.getX()) && s1.getX() <=s2.getX()) &&
+        //basic boundingBox collision detection taken from lecture slides
+        return ((s1.getX() + s1.getImage().getWidth(null) > s2.getX()) && s1.getX() <= s2.getX()) &&
                 ((s1.getY() + s1.getImage().getHeight(null) > s2.getY()) && s1.getY() < s2.getY());
     }
 
@@ -359,7 +354,6 @@ public class Game extends GameCore {
         // This method should check actual tile map collisions. For
         // now it just checks if the player has gone off the bottom
         // of the tile map.
-
         if (s.getY() + s.getHeight() > tmap.getPixelHeight()) {
             // Put the player back on the map
             s.setY(tmap.getPixelHeight() - s.getHeight());
@@ -368,55 +362,75 @@ public class Game extends GameCore {
         int tileCoordX = (int) (s.getX() / tmap.getTileWidth());
         int tileCoordY = (int) ((s.getY() + s.getHeight()) / tmap.getTileHeight());//offset by 1 so the player sits on top of the block
 
+        // here we will actually make each comparison twice, once using the lower left corner of the sprite,
+        // and once using the lower right corner of the sprite. this allows for more consistent collision detection
+        // and prevents the issue where the sprite would fall through a tile once halfway across it.
         if (tmap.getTileChar(tileCoordX, tileCoordY) == 'p' || tmap.getTileChar(tileCoordX, tileCoordY) == 'b' ||
-                tmap.getTileChar(tileCoordX+1, tileCoordY) == 'p' || tmap.getTileChar(tileCoordX+1, tileCoordY) =='b'){//if grass or dirt block touched
+                tmap.getTileChar(tileCoordX + 1, tileCoordY) == 'p' || tmap.getTileChar(tileCoordX + 1, tileCoordY) == 'b') {//if grass or dirt block touched
             if (s.getVelocityY() > 0) {
-                s.setVelocityY(0);
+                s.setVelocityY(0); //stop the sprite from falling
             }
-
             s.setY((float) (tileCoordY * tmap.getTileHeight()) - s.getHeight());
             falling = false;
             jumpsDone = 0;
         } else {
-            falling = true;
+            falling = true; //if the sprite is not touching grass or dirt, it must be falling. flag this.
         }
-        if (tmap.getTileChar(tileCoordX, tileCoordY) == 't') {
+        if (tmap.getTileChar(tileCoordX, tileCoordY) == 't' || tmap.getTileChar(tileCoordX + 1, tileCoordY) == 't') {//if touching the lava/fire blocks
             endGame();
         }
-        if (tmap.getTileChar(tileCoordX, tileCoordY) == 'f') {
+        if (tmap.getTileChar(tileCoordX, tileCoordY) == 'f' || tmap.getTileChar(tileCoordX + 1, tileCoordY) == 'f') {//if touching the finish flag
             nextLevel();
         }
-
     }
 
+    /**
+     * method to change the level once the finish flag is reached
+     */
     private void nextLevel() {
-        if (level == 1) {
+        if (level == 1) {//if the level is 1, load map 2 and set level to 2
             init("map2.txt");
             level = 2;
-        } else {
+        } else {//else if the level is 2, load the map 1 and set level to 1
             level = 1;
             init("map1.txt");
         }
     }
 
+    /**
+     * method that stops the player from moving and is dead
+     */
     private void endGame() {
         updateAnim("dead");
+
         player.setVelocityX(0);
         player.setVelocityY(0);
-        gameOver = true;
+
+        gameOver = true;//this breaks the update loop until false
+
         status = "Dead X.X";
-        Sound s = new Sound("sounds/death.wav");
+
+        Sound s = new Sound("sounds/death.wav");//play death sound
         s.start();
     }
 
-    private void resetGame() {//reset the player character to original state
-        gameOver = false;
+    /**
+     * method that resets the player character to the original state
+     */
+    private void resetGame() {
+        gameOver = false;//re-starts the game loop
+
         player.setX(20);
         player.setY(100);
+
         player.setVelocityY(0);
         player.setVelocityX(0);
+
         updateAnim("pause");
+
         status = "Alive :D";
+
+        //reset these so the player doesn't begin the game moving
         right = false;
         left = false;
     }
@@ -441,19 +455,17 @@ public class Game extends GameCore {
 
         if (key == KeyEvent.VK_F5) resetGame();
 
+
+        //keys M and N are used to test the map changing, if you can't beat the level without it ;)
         if (key == KeyEvent.VK_M) {
             level = 2;
             init("map2.txt");
-
         }
         if (key == KeyEvent.VK_N) {
             level = 1;
             init("map1.txt");
-
         }
-
     }
-
 
 
     public void keyReleased(KeyEvent e) {
